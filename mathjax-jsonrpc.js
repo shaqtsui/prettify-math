07fced99a9a3a3674ad9e46d96f00542660cbdeb
xfcjscn <xfcjscn@gmail.com>
2021-09-28 12:01:00 +0800

    minor mode, latex

--- mathjax-jsonrpc.js
+++ mathjax-jsonrpc.js
@@ -4,7 +4,7 @@ const mathjax = require('mathjax');
 const rpc = require('vscode-jsonrpc');
 
 mathjax.init({
-    loader: { load: ['input/asciimath', 'output/svg'] }
+    loader: { load: ['input/asciimath', 'input/tex', 'output/svg'] }
 }).then((MathJax) => {
     let connection = rpc.createMessageConnection(
         new rpc.StreamMessageReader(process.stdin),
@@ -12,6 +12,10 @@ mathjax.init({
 
     connection.onRequest(new rpc.RequestType('asciimath2svg'), (param) => {
         let svg = MathJax.asciimath2svg(param, { display: false });
+        return MathJax.startup.adaptor.innerHTML(svg);
+    });
+    connection.onRequest(new rpc.RequestType('tex2svg'), (param) => {
+        let svg = MathJax.tex2svg(param, { display: false });
         return MathJax.startup.adaptor.innerHTML(svg);
     });
 
--- prettify-math-mode.el
+++ prettify-math-mode.el
@@ -4,24 +4,34 @@
 (require 'jsonrpc)
 (require 'dash)
 (fset 'mathexp-to-svg (let* ((mjserver (make-process :name "mjserver"
-                                                      :buffer "mjserver"
-                                                      :command '("node" "mathjax-jsonrpc.js")
-                                                      :connection-type 'pipe
-                                                      ;; :stderr "myjservererr"
-                                                      ))
-                              (conn (jsonrpc-process-connection :process mjserver)))
-                         (set-process-query-on-exit-flag mjserver nil)
-                         (lambda (exp)
-                           (jsonrpc-request conn :asciimath2svg exp))))
+                                                     :buffer "mjserver"
+                                                     :command '("node" "mathjax-jsonrpc.js")
+                                                     :connection-type 'pipe
+                                                     ;; :stderr "myjservererr"
+                                                     ))
+                             (conn (jsonrpc-process-connection :process mjserver)))
+                        (set-process-query-on-exit-flag mjserver nil)
+                        (lambda (exp &optional type)
+                          (jsonrpc-request conn
+                                           (-> type
+                                               (or 'asciimath)
+                                               symbol-name
+                                               (concat "2svg")
+                                               make-symbol)
+                                           exp))))
 
-(defvar default-delimiter "$$")
+(defvar default-delimiters-alist
+  '(("$$" . tex)
+    ("`" . asciimath)))
 
 (defun delimiter-to-regexp (delimiter)
   (let* ((dlmt-beginning (cond ((consp delimiter) (car delimiter))
                                ((atom delimiter) delimiter)))
          (dlmt-end (cond ((consp delimiter) (cdr delimiter))
                          ((atom delimiter) delimiter))))
-    (concat (regexp-quote dlmt-beginning)
+    (concat "\\("
+            (regexp-quote dlmt-beginning)
+            "\\)"
             "\\("
             ".+?"
             "\\)"
@@ -45,33 +55,56 @@
 ;; face only specified when its value non-nil
 (defun facespec-fn ()
   (let* ((start (match-beginning 0))
-         (mathexp (match-string 1)))
+         (dlmt (match-string 1))
+         (mathexp (match-string 2)))
     (if (get-text-property start 'focus-on)
         `(face nil cursor-sensor-functions (update-focus-on)
                rear-nonsticky (cursor-sensor-functions))
       `(face nil display ((image . (:type svg
-                                          :data ,(mathexp-to-svg mathexp)
+                                          :data ,(mathexp-to-svg mathexp (assoc-default dlmt default-delimiters-alist))
                                           :scale 1.8))
                           (raise 0.4))
              cursor-sensor-functions (update-focus-on)
              rear-nonsticky (cursor-sensor-functions)))))
 
+(defvar keywords
+  (--map (list (delimiter-to-regexp (car it))
+               0
+               '(facespec-fn))
+         default-delimiters-alist))
+
+(defvar extra-properties
+  '(display cursor-sensor-functions rear-nonsticky))
 
 ;; syntax class is mostly exclusive
 ;; but $ may be used both as word & delimiter
 ;; so only keyword is suitble here
 ;; display.image is dyna computed for each content
 (defun register-in-font-lock ()
-  (setq cursor-sensor-inhibit nil)
   (cursor-sensor-mode 1)
   (setq pre-redisplay-functions (delq 'cursor-sensor--detect pre-redisplay-functions))
-  (font-lock-add-keywords nil
-                          `((,(delimiter-to-regexp default-delimiter) 0 (facespec-fn))))
-  (--> '(display cursor-sensor-functions rear-nonsticky)
+  (font-lock-add-keywords nil keywords)
+  (--> extra-properties
        (append it font-lock-extra-managed-props)
        (setq font-lock-extra-managed-props it)))
 
-(register-in-font-lock)
+(defun unregister-in-font-lock ()
+  (cursor-sensor-mode -1)
+  (font-lock-remove-keywords nil keywords)
+  (setq font-lock-extra-managed-props (--remove (memq it extra-properties)
+                                                font-lock-extra-managed-props)))
+
+
+(define-minor-mode prettify-math-mode
+  "prettify math mode base on font lock"
+  :lighter " pmath"
+  (if prettify-math-mode
+      (progn
+        (register-in-font-lock)
+        (font-lock-flush))
+    (unregister-in-font-lock)
+    (with-silent-modifications
+      (remove-list-of-text-properties (point-min) (point-max) extra-properties))))
 
 (provide 'prettify-math-mode)
 ;;; prettify-math-mode.el ends here
