aa2708837dbf6dd5c28eb9ddcf93575e32565fd4
xfcjscn <xfcjscn@gmail.com>
2021-10-02 12:31:38 +0800

    readme, rename var, global mode

--- prettify-math-mode.el
+++ prettify-math-mode.el
@@ -11,14 +11,15 @@
 (require 'jsonrpc)
 (require 'dash)
 
-(defconst pkg-base (if load-file-name (file-name-directory load-file-name) "./"))
-(setq default-directory (expand-file-name pkg-base))
+(defconst prettify-math-mode--pkg-base (if load-file-name (file-name-directory load-file-name) "./"))
+(setq default-directory (expand-file-name prettify-math-mode--pkg-base))
 
-(defun init-mathjax ()
-  (unless (file-exists-p (expand-file-name "package-lock.json" pkg-base))
+(defun prettify-math-mode--init-mathjax ()
+  (unless (file-exists-p (expand-file-name "package-lock.json" prettify-math-mode--pkg-base))
     (call-process "npm" nil "*init-mathjax*" nil "install")))
 
-(fset 'mathexp-to-svg (let* ((_ (init-mathjax))
+(declare-function prettify-math-mode--mathexp-to-svg "prettify-math-mode" t t)
+(fset 'prettify-math-mode--mathexp-to-svg (let* ((_ (prettify-math-mode--init-mathjax))
                              (mjserver (make-process :name "mjserver"
                                                      :buffer "mjserver"
                                                      :command '("node" "mathjax-jsonrpc.js")
@@ -36,11 +37,12 @@
                                                make-symbol)
                                            exp))))
 
-(defvar default-delimiters-alist
+;; can't use customize, as init depdent on it
+(defvar prettify-math-mode-delimiters-alist
   '(("$$" . tex)
     ("`" . asciimath)))
 
-(defun delimiter-to-regexp (delimiter)
+(defun prettify-math-mode--delimiter-to-regexp (delimiter)
   (let* ((dlmt-beginning (cond ((consp delimiter) (car delimiter))
                                ((atom delimiter) delimiter)))
          (dlmt-end (cond ((consp delimiter) (cdr delimiter))
@@ -54,7 +56,7 @@
             (regexp-quote dlmt-end))))
 
 
-(defun update-focus-on (window old-pos action)
+(defun prettify-math-mode--update-focus-on (window old-pos action)
   (with-silent-modifications
     (if (eq action 'entered)
         (let ((s (previous-single-property-change (1+ (point)) 'display nil (point-min)))
@@ -69,7 +71,7 @@
 
 ;; unfontify before fontify?
 ;; face only specified when its value non-nil
-(defun facespec-fn ()
+(defun prettify-math-mode--facespec-fn ()
   (let* ((start (match-beginning 0))
          (dlmt (match-string 1))
          (mathexp (match-string 2)))
@@ -77,37 +79,37 @@
         `(face nil cursor-sensor-functions (update-focus-on)
                rear-nonsticky (cursor-sensor-functions))
       `(face nil display ((image . (:type svg
-                                          :data ,(mathexp-to-svg mathexp (assoc-default dlmt default-delimiters-alist))
+                                          :data ,(prettify-math-mode--mathexp-to-svg mathexp (assoc-default dlmt prettify-math-mode-delimiters-alist))
                                           :scale 1.8))
                           (raise 0.4))
              cursor-sensor-functions (update-focus-on)
              rear-nonsticky (cursor-sensor-functions)))))
 
-(defvar keywords
-  (--map (list (delimiter-to-regexp (car it))
+(defvar prettify-math-mode--keywords
+  (--map (list (prettify-math-mode--delimiter-to-regexp (car it))
                0
                '(facespec-fn))
-         default-delimiters-alist))
+         prettify-math-mode-delimiters-alist))
 
-(defvar extra-properties
+(defvar prettify-math-mode--extra-properties
   '(display cursor-sensor-functions rear-nonsticky))
 
 ;; syntax class is mostly exclusive
 ;; but $ may be used both as word & delimiter
 ;; so only keyword is suitble here
 ;; display.image is dyna computed for each content
-(defun register-in-font-lock ()
+(defun prettify-math-mode--register-in-font-lock ()
   (cursor-sensor-mode 1)
   (setq pre-redisplay-functions (delq 'cursor-sensor--detect pre-redisplay-functions))
-  (font-lock-add-keywords nil keywords)
-  (--> extra-properties
+  (font-lock-add-keywords nil prettify-math-mode--keywords)
+  (--> prettify-math-mode--extra-properties
        (append it font-lock-extra-managed-props)
        (setq font-lock-extra-managed-props it)))
 
-(defun unregister-in-font-lock ()
+(defun prettify-math-mode--unregister-in-font-lock ()
   (cursor-sensor-mode -1)
-  (font-lock-remove-keywords nil keywords)
-  (setq font-lock-extra-managed-props (--remove (memq it extra-properties)
+  (font-lock-remove-keywords nil prettify-math-mode--keywords)
+  (setq font-lock-extra-managed-props (--remove (memq it prettify-math-mode--extra-properties)
                                                 font-lock-extra-managed-props)))
 
 ;;;###autoload
@@ -116,11 +118,16 @@
   :lighter " pmath"
   (if prettify-math-mode
       (progn
-        (register-in-font-lock)
+        (prettify-math-mode--register-in-font-lock)
         (font-lock-flush))
-    (unregister-in-font-lock)
+    (prettify-math-mode--unregister-in-font-lock)
     (with-silent-modifications
-      (remove-list-of-text-properties (point-min) (point-max) extra-properties))))
+      (remove-list-of-text-properties (point-min) (point-max) prettify-math-mode--extra-properties))))
+
+;;;###autoload
+(define-globalized-minor-mode global-prettify-math-mode
+  prettify-math-mode
+  prettify-math-mode)
 
 (provide 'prettify-math-mode)
 ;;; prettify-math-mode.el ends here
