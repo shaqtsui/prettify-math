221bdb475bef1f0fea00a78a887d9e4e45ddcd29
xfcjscn <xfcjscn@gmail.com>
2021-10-04 14:44:53 +0800

    cask

--- prettify-math-pkg.el
+++ prettify-math-pkg.el
@@ -1,9 +1,4 @@
-(define-package "prettify-math" "0.1" "prettify math like prettify symbol"
-  '((dash "2.19.0") (jsonrpc "1.0.9"))
-  :authors
-  '(("Fucheng Xu" . "xfcjscn@163.com"))
-  :maintainer
-  '("Fucheng Xu" . "xfcjscn@163.com")
-  :keywords
-  '("math" "asciimath" "tex" "latex" "2-d" "mathjax" "prettify")
-  :url "https://gitee.com/xfcjscn/prettify-math")
+(define-package "prettify-math" "0.1" "Prettify math formula"
+  '((jsonrpc "1.0.9")
+    (dash "2.19.0")
+    (emacs "25.1")))
